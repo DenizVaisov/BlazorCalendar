2aa4f382f05bc981f6eeebe323dd5de7132c2d27
DenizVaisov <deniz.vaisov@gmail.com>
2022-07-27 11:58:42 +0300

    Add EventCalendar component, removed unused files

---
+++ BlazorCalendar.Web/Data/EnumDescription.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace BlazorCalendar.Web.Data
+{
+    public static class EnumDescription 
+    {
+        public static string GetEnumDescription(this Enum e)
+        {
+            var descriptionAttribute = e.GetType().GetMember(e.ToString())[0]
+                .GetCustomAttributes(typeof(DescriptionAttribute), inherit: false)[0] 
+                as DescriptionAttribute;
+
+            return descriptionAttribute.Description;
+        }
+    }
+}
